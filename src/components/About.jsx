97bc2ff992058c3695b1cbb5fc6d646a9caa110e
HazrathSayyidMadani <hazrathschool@gmail.com>
2026-07-22 14:29:15 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import heroImage from "../assets/hero.jpg";
+
 
 
 function About() {
@@ -19,7 +19,7 @@ function About() {
           {/* Image */}
           <div>
             <img
-              src={heroImage}
+              src="/images/hero.jpg"
               alt="School Building"
               className="rounded-3xl shadow-2xl w-full"
             />
